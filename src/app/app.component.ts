15e3729a549aabc793c0eb2dc91f04a868fc13db
Nikola Mirchev <nikola.mirchev.official@gmail.com>
2023-04-23 21:50:39 +0200

    Change angular selector prefix to wf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'wf-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
